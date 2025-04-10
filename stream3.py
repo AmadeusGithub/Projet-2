6da0a0e8d40a453324befcd5d6b823e82515b5e0
AMADEÜS ANALYSIS <176412533+AmadeusGithub@users.noreply.github.com>
2025-04-10 11:35:20 +0200

    2stream3.py

--- stream3.py
+++ stream3.py
@@ -5,37 +5,6 @@ import streamlit as st
 from streamlit_authenticator import Authenticate
 from streamlit_option_menu import option_menu
 
-# Nos données utilisateurs doivent respecter ce format
-lesDonneesDesComptes = {
-    'usernames': {
-        'utilisateur': {
-            'name': 'utilisateur',
-            'password': 'utilisateurMDP',
-            'email': 'utilisateur@gmail.com',
-            'failed_login_attemps': 0,  # Sera géré automatiquement
-            'logged_in': False,  # Sera géré automatiquement
-            'role': 'utilisateur'
-        },
-        'root': {
-            'name': 'root',
-            'password': 'rootMDP',
-            'email': 'admin@gmail.com',
-            'failed_login_attemps': 0,  # Sera géré automatiquement
-            'logged_in': False,  # Sera géré automatiquement
-            'role': 'administrateur'
-        }
-    }
-}
-
-authenticator = Authenticate(
-    lesDonneesDesComptes,  # Les données des comptes
-    "cookie name",  # Le nom du cookie, un str quelconque
-    "cookie key",  # La clé du cookie, un str quelconque
-    30,  # Le nombre de jours avant que le cookie expire 
-)
-
-# Methode pour afficher les champs
-authenticator.login()
 
 def accueil():
     st.title("Bienvenu sur mon service de produits de soins naturels")
